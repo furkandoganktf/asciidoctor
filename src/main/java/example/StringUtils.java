2bfb1eff109da8ce2f38fd398aadff6f256c4469
furkan <furkandoganktf@gmail.com>
2018-07-02 13:54:44 +0300

    fisrt commit

---
+++ src/main/java/example/StringUtils.java
@@ -0,0 +1,10 @@
+package example;
+
+public class StringUtils {
+    // tag::contains[]
+    public boolean contains(String haystack, String needle) {
+        return haystack.contains(needle);
+    }
+    public int a;
+    // end::contains[]
+}
